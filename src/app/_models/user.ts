de375d53a39d2016dc65260e650bce37a04d6984
James Hall <greatwolf3d@gmail.com>
2018-06-12 22:44:22 -0700

    added user service

---
+++ src/app/_models/user.ts
@@ -0,0 +1,21 @@
+export class User {
+    id: number;
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    phoneNumber: string;
+    skills: string;
+    desciption: string;
+    picture: string;
+    teamPicture: string;
+    teamName: string;
+    zipCode: string;
+    angelListUrl: string;
+    twitterUrl: string;
+    facebookUrl: string;
+    linkedInUrl: string;
+    instagramUrl: string;
+    isVerified: string;
+    hasBypass: string;
+}
